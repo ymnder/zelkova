1600930dfc4865a6314f264c577b6feb7029a68e
ymnder <yamazaki.waseda@gmail.com>
2018-08-16 14:28:46 +0900

    fix sample class

--- app/src/main/java/com/ymnd/android/zelkova/Ingest.kt
+++ app/src/main/java/com/ymnd/android/zelkova/Ingest.kt
@@ -3,18 +3,23 @@ package com.ymnd.android.zelkova
 import com.ymnd.android.annotation.Builder
 import com.ymnd.android.annotation.DefaultValue
 
-//@Builder
+@Builder
 data class Ingest(
         @DefaultValue(defaultValue = true)
         var index: Int?,
         @DefaultValue
-        val category: Long,
+        val millSec: Long,
         @DefaultValue(defaultValue = false)
         var name: String,
         val action: Action,
-        var id: Long
+        var id: Long,
+        var category: Category
 )
 
 data class Action(
+        val id: Int
+)
+
+data class Category(
         val id: Int
 )

8fda598ae1d5fc05d4337f1613c1811a8b982766
ymnder <yamazaki.waseda@gmail.com>
2018-09-22 23:15:16 +0900

    sampleで利用した

--- app/src/main/java/com/ymnd/android/zelkova/MainActivity.kt
+++ app/src/main/java/com/ymnd/android/zelkova/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.ymnd.android.zelkova
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,6 +9,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val build = TeaBuilder()
+                .category(arrayListOf())
+                .id(0)
+                .name("EarlGrey")
+                .price(1000)
+                .build()
+        Log.v("TEST", build.toString())
+        Log.v("TEST", teaBuilder {
+            id(100)
+            name("Assam")
+            price(1200)
+            category(arrayListOf())
+        }.toString())
 //        ingestBuilder(0, Action(0)){
 //            index(0)
 //        }

f81c7f4e0cfe27ef76abe966e5af0ae67e3aeb66
ymnder <yamazaki.waseda@gmail.com>
2018-09-22 21:22:36 +0900

    kotlin builder

--- processor/src/main/java/com/ymnd/android/processor/KotlinBuilderProcessor.kt
+++ processor/src/main/java/com/ymnd/android/processor/KotlinBuilderProcessor.kt
@@ -3,10 +3,13 @@ package com.ymnd.android.processor
 import com.google.auto.service.AutoService
 import com.squareup.kotlinpoet.*
 import com.ymnd.android.annotation.Builder
-import me.eugeniomarletti.kotlin.metadata.*
+import me.eugeniomarletti.kotlin.metadata.ClassData
+import me.eugeniomarletti.kotlin.metadata.KotlinClassMetadata
+import me.eugeniomarletti.kotlin.metadata.extractFullName
+import me.eugeniomarletti.kotlin.metadata.kotlinMetadata
 import me.eugeniomarletti.kotlin.metadata.shadow.metadata.ProtoBuf
-import me.eugeniomarletti.kotlin.metadata.shadow.metadata.deserialization.NameResolver
 import java.io.File
+import java.util.*
 import javax.annotation.processing.AbstractProcessor
 import javax.annotation.processing.Processor
 import javax.annotation.processing.RoundEnvironment
@@ -36,119 +39,112 @@ class KotlinBuilderProcessor : AbstractProcessor() {
     }
 
     private fun generateClassFile(targetElement: Element) {
-        val typeSpecBuilder = TypeSpec
-                .classBuilder("${targetElement.simpleName}Builder")
-                .addModifiers(KModifier.OPEN)
+        val className = "${targetElement.simpleName}"
+        val fileName = "${className}Builder"
+        val packageName = "${processingEnv.elementUtils.getPackageOf(targetElement)}"
+        val targetClassName = targetElement.asType().asTypeName()
 
         val metaData = (targetElement.kotlinMetadata as KotlinClassMetadata).data
-        val typeElement = targetElement as TypeElement
         val properties: List<ProtoBuf.Property> = metaData.classProto.propertyList
-        val tparams: List<ProtoBuf.TypeParameter> = metaData.classProto.typeParameterList
-        val name: String = typeElement.simpleName.toString()
-        val pairs: List<Triple<String, String, String>> = properties.map { protbuf ->
-            val retType = protbuf.returnType.extractFullName(metaData, true)
-            val imports = protbuf.returnType.extractFullImport(metaData, true)
-//                        it.isVal
-//                        it.isVar
-//                        it.returnType.nullable
-            val pname = metaData.nameResolver.getString(protbuf.name)
-            //これは最適化できそう
-            val importsList = imports
-                    .removeBackticks()
-                    .split(",")
-                    .map { it.trim() }
-                    .distinct()
-                    .joinToString(prefix = "import ", postfix = " \n", separator = " \n import ")
-            Triple(name, retType.removeBackticks(), "var")
+        val fieldTypes: List<FieldType> = properties.map { property ->
+            FieldType(property, metaData)
         }
-        pairs.forEach { pair ->
+
+        val typeSpecBuilder = TypeSpec
+                .classBuilder(fileName)
+                .addModifiers(KModifier.OPEN)
+
+        fieldTypes.forEach { fieldType ->
             typeSpecBuilder.addProperty(
-                    PropertySpec.builder(pair.first, generateClassName(pair.second))
+                    PropertySpec.varBuilder(fieldType.fieldName, fieldType.className)
                             .addModifiers(KModifier.PRIVATE)
-                            .initializer(pair.first)
+                            .apply {
+                                if (fieldType.nullable) {
+                                    this.initializer("null")
+                                } else {
+                                    this.delegate("kotlin.properties.Delegates.notNull()")
+                                }
+                            }
+                            .build()
+            )
+
+            typeSpecBuilder.addFunction(
+                    FunSpec.builder(fieldType.fieldName)
+                            .addParameter(fieldType.fieldName, fieldType.className)
+                            .addStatement("return apply { this.%N = %N }",
+                                    fieldType.fieldName, fieldType.fieldName
+                            )
                             .build()
             )
         }
-        val className = targetElement.simpleName.toString()
-        val pack = processingEnv.elementUtils.getPackageOf(targetElement).toString()
-        //generateClass(className, pack)
+
+        val fieldNames = arrayListOf<String>().apply {
+            fieldTypes.forEach { fieldType ->
+                val fieldName = fieldType.fieldName
+                add(fieldName)
+                add(fieldName)
+            }
+        }
+
+        typeSpecBuilder.addFunction(
+                FunSpec.builder("build")
+                        .addStatement("return %T(" + Collections.nCopies(fieldTypes.size, "%N = %N")
+                                .joinToString() + ")",
+                                targetClassName,
+                                *fieldNames.toTypedArray()
+                        )
+                        .build()
+        )
+
+        val lambdaTypeName = LambdaTypeName.get(receiver = ClassName(packageName, "${fileName}Dsl"), returnType = Unit::class.java.asTypeName())
+        val lambda = ParameterSpec.builder("f", lambdaTypeName).build()
+
+        val dslTypeSpec = TypeSpec.classBuilder("${fileName}Dsl")
+                .superclass(ClassName(packageName, fileName))
+                .build()
+
+        val dslFunSpec = FunSpec.builder(fileName.decapitalize())
+                .addModifiers(KModifier.INLINE)
+                .addParameter(lambda)
+                .addStatement(
+                        "return "
+                                + "${fileName}Dsl("
+                                + ").apply{ f() }"
+                                + ".build()")
+                .build()
 
         val typeSpec = typeSpecBuilder.build()
-        FileSpec.builder(PACKAGE_NAME, typeSpec.name!!)
+
+        val outputDirectory = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME]
+                ?.replace("kaptKotlin", "kapt")
+                ?.let { File(it) }
+                ?: throw IllegalArgumentException("No output directory...")
+
+        FileSpec.builder(packageName, typeSpec.name!!)
                 .addType(typeSpec)
+                .addType(dslTypeSpec)
+                .addFunction(dslFunSpec)
                 .build()
-                .writeTo(processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME]!!.let { File(it) })
+                .writeTo(outputDirectory)
 
     }
 
-    private fun generateClassName(className: String): ClassName {
-        try {
-            return ClassName.bestGuess(className)
-        } catch (e: IllegalArgumentException) {
-            return ClassName.bestGuess("kotlin.collections.List<kotlin.String>")
-        }
-    }
+    class FieldType(
+            property: ProtoBuf.Property,
+            metaData: ClassData
+    ) {
+        private val returnType = property.returnType.extractFullName(metaData, true)
+        private val simpleType = returnType
+                .replace("`", "")
+                .replace("kotlin.collections.", "")
+                .replace("kotlin.", "")
 
-    fun String.removeBackticks() = this.replace("`", "")
-
-    private fun generateClass(className: String, pack: String) {
-        val fileName = "Generated_$className"
-        val file = FileSpec.builder(pack, fileName)
-                .addType(TypeSpec.classBuilder(fileName)
-                        .addFunction(FunSpec.builder("getName")
-                                .addStatement("return \"World\"")
-                                .build())
-                        .build())
-                .build()
-
-        val kaptKotlinGeneratedDir = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME]
-        file.writeTo(File(kaptKotlinGeneratedDir, "$fileName.kt"))
+        val className = ClassName.bestGuess(simpleType)
+        val fieldName = metaData.nameResolver.getString(property.name)
+        val nullable = property.returnType.nullable
     }
 
     companion object {
         const val KAPT_KOTLIN_GENERATED_OPTION_NAME = "kapt.kotlin.generated"
-        private const val PACKAGE_NAME = "com.ymnd.android.builder"
     }
-
-    fun ProtoBuf.Type.extractFullImport(
-            nameResolver: NameResolver,
-            getTypeParameter: (index: Int) -> ProtoBuf.TypeParameter,
-            outputTypeAlias: Boolean = true,
-            throwOnGeneric: Throwable? = null
-    ): String {
-
-        if (!hasClassName() && throwOnGeneric != null) throw throwOnGeneric
-
-        val name = when {
-            hasTypeParameter() -> getTypeParameter(typeParameter).name
-            hasTypeParameterName() -> typeParameterName
-            outputTypeAlias && hasAbbreviatedType() -> abbreviatedType.typeAliasName
-            else -> className
-        }.let { nameResolver.getString(it).escapedClassName }
-
-        val argumentList = when {
-            outputTypeAlias && hasAbbreviatedType() -> abbreviatedType.argumentList
-            else -> argumentList
-        }
-        val arguments = argumentList
-                .takeIf { it.isNotEmpty() }
-                ?.joinToString(prefix = "", postfix = "") {
-                    when {
-                        it.hasType() -> it.type.extractFullImport(nameResolver, getTypeParameter, outputTypeAlias, throwOnGeneric)
-                        throwOnGeneric != null -> throw throwOnGeneric
-                        else -> ""
-                    }
-                }
-                ?: ""
-
-        return if (!arguments.isEmpty()) "$name,$arguments" else name
-    }
-
-    fun ProtoBuf.Type.extractFullImport(
-            data: ClassData,
-            outputTypeAlias: Boolean = true,
-            throwOnGeneric: Throwable? = null
-    ) =
-            extractFullImport(data.nameResolver, data.proto::getTypeParameter, outputTypeAlias, throwOnGeneric)
-
 }
